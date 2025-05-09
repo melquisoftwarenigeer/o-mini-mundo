c65b179c6cedfde5a33c3ab837a344664a466e33
melquisoftwarenigeer <melquij5pro@gmail.com>
2025-05-08 23:27:08 -0300

    Adiciona funcionalidade de logout modularizada

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { setupLogoutButton } from './logout'; // Importa a função de logout
+
+// Configura o botão de logout quando a página for carregada
+document.addEventListener('DOMContentLoaded', () => {
+    setupLogoutButton(); // Configura o evento de logout
+});
